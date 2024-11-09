ff0f7ab16ba0bfd5c0a953d281735d1eca0ed308
Daa Nkwa <akrasikonadu@yahoo.com>
2024-11-09 22:16:26 +0000

    fixed 3rd card, remain show contact info button

--- js/scripts.js
+++ js/scripts.js
@@ -72,17 +72,36 @@ function displayPatientData(patient) {
   // Display other patient data
   const patientInfo = document.getElementById('patient-info');
   patientInfo.innerHTML = `
-    <img src="${patient.profile_picture}" alt="${patient.name}" class="w-10 h-10 rounded-full mr-4" />
-    <p><strong></strong> ${patient.name}</p>
-    <p><strong>Date of Birth:</strong> ${patient.date_of_birth}</p>
-    <p><strong>Gender:</strong> ${patient.gender}</p>
-    <p><strong>Contact Info:</strong> ${patient.phone_number}</p>
-    <p><strong>Emergency Contact:</strong> ${patient.emergency_contact}</p>
-    <p><strong>Insurance provider:</strong> ${patient.insurance_type}</p>
-  `;
+    <img src="${patient.profile_picture}" alt="${patient.name}" class="w-40 h-40 rounded-full mx-auto mb-4" />
+    <p class="text-center font-bold text-xl">${patient.name}</p>
+    <p><img src="images/BirthIcon@2x.png" alt="Calendar" class="w-4 h-4 inline mr-2"><span class="inline-flex flex-col">
+        <span>Date of Birth:</span>
+        <span class="text-sm font-semibold ml-[88px]">${patient.date_of_birth}</span>
+      </span></p>
+    <p><img src="images/FemaleIcon@2x.png" alt="Gender" class="w-4 h-4 inline mr-2"><span class="inline-flex flex-col">
+        <span>Gender:</span>
+        <span class="text-sm font-semibold ml-[88px]">${patient.gender}</span>
+      </span></p>
+    <p><img src="images/PhoneIcon@2x.png" alt="Phone" class="w-4 h-4 inline mr-2"><span class="inline-flex flex-col">
+        <span>Contact Info:</span>
+        <span class="text-sm font-semibold ml-[88px]">${patient.phone_number}</span>
+      </span></p>
+    <p><img src="images/PhoneIcon@2x.png" alt="Emergency" class="w-4 h-4 inline mr-2"><span class="inline-flex flex-col">
+        <span>Emergency Contact:</span>
+        <span class="text-sm font-semibold ml-[88px]">${patient.emergency_contact}</span>
+      </span></p>
+    <p><img src="images/InsuranceIcon@2x.png" alt="Insurance" class="w-4 h-4 inline mr-2"><span class="inline-flex flex-col">
+        <span>Insurance provider:</span>
+        <span class="text-sm font-semibold ml-[88px]">${patient.insurance_type}</span>
+      </span></p>
+    <br>
+    <br>
+    
+    <button class="w-[220px] h-[41px] rounded-full bg-[blue] text-black">Show all information</button>
+    `;
 }
 
-function listPatients(patientsData) {
+  function listPatients(patientsData) {
   const patientList = document.getElementById('patient-list');
   if (!patientsData || !Array.isArray(patientsData)) {
     patientList.innerHTML = '<li>No patients found</li>';
